91e7931b89899054b05c60bac179b82a25c265fb
Erdoğan BULUT <e-bulut@hotmail.com.tr>
2019-11-18 16:32:04 +0300

    Remove dummy js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 global.$ = require("jquery");
 
-// Test import of a JavaScript function, an SVG, and Sass
-import HelloWorld from './js/HelloWorld';
 import './styles/NTCUIKit.css';
 import './styles/index.scss';
 
-// Create heading node
-const greeting = document.createElement('h1');
-greeting.textContent = HelloWorld();
-
 // Append SVG and heading nodes to the DOM
 const app = document.querySelector('#root');
-app.append(greeting);
+app.append();
