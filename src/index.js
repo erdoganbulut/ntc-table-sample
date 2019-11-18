0edcd5622ca89b692f74a96604c524bcdef9f35a
Erdoğan BULUT <e-bulut@hotmail.com.tr>
2019-11-18 16:26:58 +0300

    Remove logo

--- src/index.js
+++ src/index.js
@@ -2,13 +2,8 @@ global.$ = require("jquery");
 
 // Test import of a JavaScript function, an SVG, and Sass
 import HelloWorld from './js/HelloWorld';
-import WebpackLogo from './images/webpack-logo.svg';
 import './styles/NTCUIKit.css';
 import './styles/index.scss';
-
-// Create SVG logo node
-const logo = document.createElement('img');
-logo.src = WebpackLogo;
 
 // Create heading node
 const greeting = document.createElement('h1');
@@ -16,4 +11,4 @@ greeting.textContent = HelloWorld();
 
 // Append SVG and heading nodes to the DOM
 const app = document.querySelector('#root');
-app.append(logo, greeting);
+app.append(greeting);
